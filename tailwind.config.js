fb7c3d67f8bd77642383f2c904b47278670a0a81
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2023-07-10 23:13:17 -0500

    add: secciones en index mobile hasta tipos asesorias

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#03A09A",
+        "primary-dark": "#026965",
+        "secondary": "#AD3405",
+        "terciary": "#CE8569",
+        "grey": "#4F5665",
+        "brown": "#443F35",
+      },
+      backgroundImage: {
+        background: 'url("../images/background.jpg")',
+      },
+      fontFamily: {
+        bebasNeue: "bebasNeue",
+        foregen: "foregen"
+      },
+    },
   },
   plugins: [],
 }
