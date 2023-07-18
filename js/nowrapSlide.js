d9909f3d8d57e6f6b481cb80222ecee4a1ad4a69
oscarfgutierrezo <oscarfgutierrezo@gmail.com>
2023-07-18 11:55:37 -0500

    Add: en pagina home, version tablet y desktop para carrusel

--- js/nowrapSlide.js
+++ js/nowrapSlide.js
@@ -1,21 +1,20 @@
-const nowrapSlide = ( containerId ) => {
+const nowrapSlide = ( containerId, scroll ) => {
     const container = document.getElementById(containerId);
-    console.log(container);
-    const images = container.querySelector(".slide-container")
-    console.log(images);
+    const images = container.querySelector(".slide-container");
     const prevBtn = container.querySelector(".prev-btn");
     const nextBtn = container.querySelector(".next-btn");
+    prevBtn.disabled = false
 
     const leftSlide = () => {
         images.scrollBy({
-            left: -200, // Valor para ajustar el desplazamiento
+            left: -scroll, // Valor para ajustar el desplazamiento
             behavior: 'smooth'
         });
     }
 
     const rightSlide = () => {
         images.scrollBy({
-            left: 200, // Valor para ajustar el desplazamiento
+            left: scroll, // Valor para ajustar el desplazamiento
             behavior: 'smooth'
         });
     }
@@ -24,5 +23,11 @@ const nowrapSlide = ( containerId ) => {
     nextBtn.addEventListener("click", rightSlide);
 }
 
-nowrapSlide("slide-asesorias-fotos")
-nowrapSlide("slide-asesorias-testimoniales")
+if (window.location.pathname === '/asesorias.html') {
+    nowrapSlide("slide-asesorias-fotos", 200);
+    nowrapSlide("slide-asesorias-testimoniales", 200);
+} else if (window.location.pathname === '/' && (window.innerWidth >= 1024)) {
+    nowrapSlide("slide-home-fotos", 400)
+};
+
+console.log(window.location.pathname);
