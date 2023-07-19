66d3c6aa2fa282365a09630848cc5ce77cd8c5d6
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2023-07-18 22:32:50 -0500

    Add: modal con form de registro

---
+++ js/abrirModal.js
@@ -0,0 +1,27 @@
+const abrirModal = ( btnId, modalId ) => {
+  const modal = document.getElementById(modalId);
+  const openBtns = document.querySelectorAll(`.${btnId}`);
+  const closeModal = modal.querySelector('.close-modal-btn')
+
+  console.log(openBtns);
+
+  const abrirModal = () => {
+    modal.classList.add('flex');
+    modal.classList.remove('hidden');
+  }
+
+  const cerrarModal = () => {
+    modal.classList.add('hidden');
+    modal.classList.remove('flex');
+  }
+
+  /* openBtn.addEventListener("click", abrirModal); */
+  closeModal.addEventListener("click", cerrarModal);
+  openBtns.forEach( btn => {
+    btn.addEventListener("click", abrirModal);
+  })
+}
+
+if (window.location.pathname === '/') {
+  abrirModal("modal-form-home-btn", "modal-form-home");
+}
