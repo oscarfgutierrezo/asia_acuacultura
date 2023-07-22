afa4a0218a923401b3834599a6dfd6dd92617363
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2023-07-22 14:24:50 -0500

    Change: imagen refractometro

--- js/data/refractometros.js
+++ js/data/refractometros.js
@@ -1,6 +1,6 @@
 export const refractometros = [
   {
-    ref: "Salinómetro Refractómetro",
+    ref: "Refractómetro Portatil",
     descripcion: 'Refractómetro, tres pipetas Pasteur de 3ml, destornillador, paño de limpieza, instructivo y caja de presentación. Ofrece Compensación automática temperatura (ATC)',
     parametros: 'Salinidad del agua',
     rango: 'Partes por mil (o/oo) 0 a 100. Gravedad especifica (d 20/20) 1.000 – 1.070',
